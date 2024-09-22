f9e212d0d8067f1de1a77f2b3281667cea91a206
Hyejin Leem <jin2im@gmail.com>
2024-09-22 00:28:37 -0700

    add 404

--- js/main.js
+++ js/main.js
@@ -12,7 +12,87 @@ document.addEventListener('DOMContentLoaded', function() {
     },
   });
 
+  // city-select
+  const cityText = document.querySelector('.city-text');
+  const cityOptions = document.querySelectorAll('.city-option');
+  const cityOptionContainer = document.querySelector('.city-option-container');
+
+  cityOptions.forEach(option => {
+    option.addEventListener('click', () => {
+      const selectedCity = option.querySelector('p').innerText;
+      const selectedIcon = option.querySelector('span').outerHTML;
+
+      cityText.innerHTML = `
+        ${selectedIcon}
+        ${selectedCity}
+      `;
+      
+      cityOptionContainer.classList.remove('active');
+    });
+  });
+
+  // city-text : add click event
+  cityText.addEventListener('click', (event) => {
+    event.stopPropagation(); // 클릭 이벤트 전파 방지
+    cityOptionContainer.classList.toggle('active');
+  });
+
+  document.addEventListener('click', () => {
+    cityOptionContainer.classList.remove('active');
+  });
+
+  // DatePicker
+  // flatpickr
+  function initializeFlatpickr(showMonthsCount) {
+    flatpickr("#datepicker", {
+      mode: "range", 
+      dateFormat: "Y-m-d", 
+      showMonths: showMonthsCount,
+      minDate: "today",
+      onChange: function(selectedDates, dateStr, instance) {
+        if (selectedDates.length === 2) {
+          const checkinDate = selectedDates[0];
+          const checkoutDate = selectedDates[1];
+          
+          document.getElementById('checkin-input').value = checkinDate.toLocaleDateString();
+          document.getElementById('checkout-input').value = checkoutDate.toLocaleDateString();
+        }
+      }
+    });
+  }
   
+  function setShowMonths() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 768) {
+      return 1; 
+    } else {
+      return 2; 
+    }
+  }
+  
+  initializeFlatpickr(setShowMonths());
+  
+
+  window.addEventListener('resize', function() {
+    document.querySelector("#datepicker")._flatpickr.destroy(); // 기존 flatpickr 인스턴스 삭제
+    initializeFlatpickr(setShowMonths()); // 다시 초기화
+  });
+
+  // guests
+  const increaseButton = document.getElementById("increase");
+  const decreaseButton = document.getElementById("decrease");
+  const countGuestInput = document.getElementById("guests-counter");
+
+  increaseButton.addEventListener("click", function(){
+    countGuestInput.stepUp();
+  });
+
+  decreaseButton.addEventListener("click", function(){
+    countGuestInput.stepDown();
+  });
+
+
+  // tabSlide
   var tabSlide = new Swiper(".tab-slide", {
     slidesPerView: 2,
     spaceBetween: 10,
